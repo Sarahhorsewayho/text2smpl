0f0cb168a7ee85d3a9fcde5b8a555bea16aa4ce8
Sarahhorsewayho <harryhe0706@gmail.com>
2019-05-18 15:06:18 +0800

    update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import lib.caption_process as cp
 
 from lib.config import cfg
 from lib.model import Model
+#from lib.summary import create_summary
 from lib.optimiser import Optimiser
 from lib.load_data import Preprocessor
 
@@ -49,7 +50,6 @@ def main():
 
 	opt.prepare_loss_ops()
 	loss_full = opt.get_loss_op()
-	losses = opt.get_loss_dict()
 
 	global_step = tf.Variable(
 		name="global_step",
@@ -58,35 +58,41 @@ def main():
 		trainable=False,
 		initial_value=0)
 
+	output = tf.Variable(
+		name="output_node",
+		expected_shape=(226,),
+		dtype=tf.float64,
+		trainable=False,
+		initial_value=0)
+
 	saver = tf.train.Saver(max_to_keep=150)
 
 	with tf.Session() as sess:
 
 		max_steps = steps_per_epoch * cfg.CONST.max_epochs
+		print("steps_per_epoch: " + str(steps_per_epoch) + " max_epochs: " + str(cfg.CONST.max_epochs) + " max_steps: " + str(max_steps))
 		total_examples_presented = num_of_data * cfg.CONST.max_epochs
-
 		train_op = opt.prepare_train_op(global_step, max_steps)
-
 		outputs = model.get_outputs()
-
+		writer = tf.summary.FileWriter("logs/", sess.graph)
 		initializer = tf.global_variables_initializer()
 		sess.run(initializer)
 		step = 0
 		preprocessor.initialise_iterator(sess)
 
 		try:
-
-			while step < max_steps:
-
-				results = sess.run(train_op)
+			while step <= 20000:
+				fetch = {"train_op": train_op, "loss: ": loss_full, "learning_rate: ": opt.learning_rate, "global_step": global_step}
+				results = sess.run(fetch)
 				step += 1
+				print("step: " + str(step) + " RESULTS: " + str(results))
+				#print("step: " + str(step) + " loss: " + str(loss_full) + " learning_rate: " + str(opt.learning_rate))
 		except tf.errors.OutOfRangeError:
 			pass
 
 		saver.save(sess,
                     cfg.DIR.checkpoint_path,
                     global_step=global_step)
-
 		print("shut down.")
 
 if __name__ == '__main__':
