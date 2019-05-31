6df8750c72c1e3718b083f1da2e7d3fa587ef679
Sarahhorsewayho <harryhe0706@gmail.com>
2019-05-31 14:00:52 +0800

    update 0531

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import lib.caption_process as cp
 
 from lib.config import cfg
 from lib.model import Model
-#from lib.summary import create_summary
 from lib.optimiser import Optimiser
 from lib.load_data import Preprocessor
+from lib.utils import open_csv
+from lib.write_output import write_to_mesh
 
 def main():
 
@@ -22,7 +23,6 @@ def main():
 	smplparams_std = np.load(cfg.DIR.smplparams_std)
 
 	param_selection = []
-	param_selection += range(10)
 	param_selection += range(10, 226)
 
 	latent_mean = smplparams_mean[param_selection]
@@ -37,6 +37,11 @@ def main():
 
 	model_inputs = batch_data['embeddings']
 	targets = batch_data['body_params']
+
+	test_dict = open_csv(cfg.DIR.test_caption_path)
+	description = test_dict['description']
+
+	caption_post = cp.gen_captions(description)
 
 	model = Model(model_inputs,
 				  tf.constant(latent_mean, dtype=np.float64),
@@ -58,17 +63,9 @@ def main():
 		trainable=False,
 		initial_value=0)
 
-	output = tf.Variable(
-		name="output_node",
-		expected_shape=(226,),
-		dtype=tf.float64,
-		trainable=False,
-		initial_value=0)
-
 	saver = tf.train.Saver(max_to_keep=150)
 
 	with tf.Session() as sess:
-
 		max_steps = steps_per_epoch * cfg.CONST.max_epochs
 		print("steps_per_epoch: " + str(steps_per_epoch) + " max_epochs: " + str(cfg.CONST.max_epochs) + " max_steps: " + str(max_steps))
 		total_examples_presented = num_of_data * cfg.CONST.max_epochs
@@ -81,18 +78,23 @@ def main():
 		preprocessor.initialise_iterator(sess)
 
 		try:
-			while step <= 20000:
-				fetch = {"train_op": train_op, "loss: ": loss_full, "learning_rate: ": opt.learning_rate, "global_step": global_step}
+			while step <= max_steps:
+				fetch = {"train_op": train_op, "loss": loss_full, "learning_rate": opt.learning_rate, "global_step": global_step}
 				results = sess.run(fetch)
 				step += 1
-				print("step: " + str(step) + " RESULTS: " + str(results))
-				#print("step: " + str(step) + " loss: " + str(loss_full) + " learning_rate: " + str(opt.learning_rate))
+				if (step % 100 == 0):
+					print("step: " + str(step) + " RESULTS: " + str(results))
+				if (step % 50000 == 0):
+					saver.save(sess,os.path.join(cfg.DIR.checkpoint_path,str(results['global_step']),"Model"),global_step=global_step)
+				if(results['loss'] < 5e-15) : 
+					break
+				#if (results['loss'] < 0.13):
+				#	break
 		except tf.errors.OutOfRangeError:
 			pass
-
-		saver.save(sess,
-                    cfg.DIR.checkpoint_path,
-                    global_step=global_step)
+		saver.save(sess,os.path.join(cfg.DIR.checkpoint_path,str(results['global_step']),"Model"),global_step=global_step)
+		predicted = sess.run(outputs, feed_dict={model_inputs: caption_post})
+		write_to_mesh(predicted, latent_mean, latent_std)
 		print("shut down.")
 
 if __name__ == '__main__':
